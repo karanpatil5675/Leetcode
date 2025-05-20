8152ef5cd2f10e41883a65b336f29a53e7a63e52
karanpatil5675 <karanpatil5675@gmail.com>
2025-05-20 09:23:52 +0530

    Time: 0 ms (100%), Space: 44.8 MB (83.35%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -8,18 +8,15 @@ public class Solution {
     }
 
     private void dfs(int[][] image, int r, int c, int color, int newColor) {
-        // Check bounds and color match
         if (r < 0 || c < 0 || r >= image.length || c >= image[0].length || image[r][c] != color) {
             return;
         }
 
-        // Change color
         image[r][c] = newColor;
 
-        // Recursive calls in 4 directions
-        dfs(image, r + 1, c, color, newColor); // down
-        dfs(image, r - 1, c, color, newColor); // up
-        dfs(image, r, c + 1, color, newColor); // right
-        dfs(image, r, c - 1, color, newColor); // left
+        dfs(image, r + 1, c, color, newColor); 
+        dfs(image, r - 1, c, color, newColor); 
+        dfs(image, r, c + 1, color, newColor); 
+        dfs(image, r, c - 1, color, newColor); 
     }
 }
